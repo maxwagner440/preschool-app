9e1e89c180642328886f0e882c080d237f5067ab
Max Wagner <maxwagner440@gmail.com>
2025-06-09 08:32:19 -0400

    using ngx-extended-pdf-viewer to scroll sideways

--- frontend/src/app/domain/pdf/display/display.component.ts
+++ frontend/src/app/domain/pdf/display/display.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { DocumentSignComponent } from '../document-sign/document-sign.component';
-
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 
 @Component({
   selector: 'app-display',
-  imports: [PdfViewerModule, DocumentSignComponent],
+  imports: [PdfViewerModule, DocumentSignComponent, NgxExtendedPdfViewerModule],
   standalone: true,
   templateUrl: './display.component.html',
   styleUrl: './display.component.scss',
@@ -15,13 +15,17 @@ import { DocumentSignComponent } from '../document-sign/document-sign.component'
 export class DisplayComponent { 
   // zoom = 1.0;
   pdfUrl = '/assets/ParentManual25-26.pdf'; // Use your own path here!
+  currentPage = 1;
+  isMobile = window.innerWidth <= 700 ? 'true' : 'false';
 
-  // ngOnInit() {
-  //   if (window.innerWidth <= 600) {
-  //     this.zoom = 1; // adjust to make it more readable on mobile
-  //   }
-  // }
 
+  previousPage() {
+    if (this.currentPage > 1) this.currentPage--;
+  }
+  
+  nextPage() {
+    this.currentPage++;
+  }
 
   downloadOriginalPdf() {
       const link = document.createElement('a');
